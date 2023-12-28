898f062b5193430e077c76c99776185a59d25b33
athira k v <athirasreemadhavan@gmail.com>
2023-12-28 16:31:19 +0530

    videoStackServer

---
+++ index.js
@@ -0,0 +1,26 @@
+//import json server library
+const jsonServer=require('json-server')
+
+// cors import 
+const cors=require('cors')
+
+
+
+//create server
+const videoServer=jsonServer.create()
+
+//create a router to connect the json file
+const router=jsonServer.router('data.json')
+
+//create middleware(json to js)
+const middleware=jsonServer.defaults()
+
+//connect with client
+videoServer.use(cors())
+videoServer.use(middleware)
+videoServer.use(router)
+
+const PORT=4000
+videoServer.listen(PORT,()=>{
+    console.log(`___Video Server Started At  Port ${PORT}___`);
+})
